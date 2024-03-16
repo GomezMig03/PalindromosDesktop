4d6ff5a0e31268090b751efce89c2247e33b149b
Miguel <gomezmig03@gmail.com>
2024-03-17 00:56:56 +0100

    Change: fix the code, so it's the same as web version.

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const inp=document.getElementById("text-input")
 const bot=document.getElementById("check-btn")
 const result=document.getElementById("result")
 
+inp.select()
+
 inp.addEventListener("keydown", (event) => {
-    var empty=false;
     if (event.key === "Enter") {
-        bot.click()
+        detectPal()
     }
 })
 
@@ -15,22 +16,32 @@ inp.addEventListener("keyup", (event) => {
     }
 })
 
-bot.addEventListener("click", () => {
+bot.addEventListener("mousedown", () => {
+    detectPal()
+})
+
+bot.addEventListener("mouseup", () => {
+    bot.style.transform="scale(1)"
+})
+
+const detectPal = ()=> {
     let text=inp.value
     if (text === "") {
-        alert("Por favor escribe un valor")
+        result.style.color = "red"
+        result.innerHTML="Por favor, Introduzca un valor."
+        bot.style.transform = "scale(0.97)"
     } else {
+        result.style.color = "black"
         bot.style.transform = "scale(0.97)"
         if (isPal(text)) {
             result.innerHTML = "<strong>" + text + "</strong> es palíndromo"
         } else {
             result.innerHTML = "<strong>" + text + "</strong> no es palíndromo"
         }
-        result.style.visibility = "visible"
     }
+    result.style.visibility = "visible"
     inp.value=""
-})
-
+}
 
 const isPal = (text) => {
     text=text.replace(/[^A-Za-z0-9]/gi, '').toLowerCase()
